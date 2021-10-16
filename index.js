18b29aaecab0d250692d1dd78acb2644177dc66c
Botond Kulcsar <bkulcsar@gmx.com>
2021-10-16 11:59:06 +0300

    exercise 4.1. application runs

---
+++ index.js
@@ -0,0 +1,48 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = 'mongodb+srv://kecs:1gumibaba2@cluster0.twaol.mongodb.net/bloglist?retryWrites=true&w=majority'
+mongoose.connect(mongoUrl)
+    .then(() => console.log(`connected to DB`))
+    .catch(err => {
+        console.log(`failed to connect to DB. Exiting...`)
+        process.exit(1)
+    })
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})

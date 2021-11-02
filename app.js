5057fed319cc56b778d45c6b39706701e1d73e24
Botond Kulcsar <bkulcsar@gmx.com>
2021-11-02 20:07:34 +0200

    prepared for deploy

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
-const cors = require('cors')
+// const cors = require('cors')
 
 const mongoose = require('mongoose')
 
@@ -18,7 +18,8 @@ mongoose.connect(config.MONGODB_URI)
         process.exit(1)
     })
 
-app.use(cors())
+// app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
